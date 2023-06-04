86871af253f0492861f6f16891cdc621dd802790
Julia Grzegorzewska <julek0309@gmail.com>
2023-06-04 14:41:27 +0200

    main file

--- Main.py
+++ Main.py
@@ -1,24 +1,15 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
-from PyQt5.QtGui import *
-import sys
-from PyQt5.QtWidgets import QApplication, QCheckBox, QMessageBox, QMainWindow, QWidget, QGridLayout, QLineEdit, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel
 from PyQt5.QtWidgets import QPushButton, QVBoxLayout
-from PyQt5.QtCore import Qt
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as canvas
-from functools import partial
 import qtmodern.styles
 import qtmodern.windows
-import numpy as np
-import random
 import sys
 from PyQt5.QtGui import QFont, QFontDatabase
 from PyQt5.QtWidgets import QDesktopWidget
-import PyQt5.QtGui
-from PyQt5.QtCore import QCoreApplication
-
-
+from InstructionsPopup import InstructionsPopup
+from RecipesPopup import RecipesPopup
+from MainPopup import MainPopup
 
 class MainWindow(QMainWindow):
     """
