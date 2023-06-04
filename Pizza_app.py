7c71734d8392b829f7720e9fb5d10639092dd02d
Julia Grzegorzewska <julek0309@gmail.com>
2023-06-04 15:57:36 +0200

    spojnosc pizz

--- Pizza_app.py
+++ Pizza_app.py
@@ -311,80 +311,66 @@ class MainNeapol(QMainWindow):
         anta = QFontDatabase.applicationFontFamilies(font1)
         bakerie = QFontDatabase.applicationFontFamilies(font2)
 
-        pagelayout = QVBoxLayout()
+        pagelayout = QGridLayout()
 
-        hbox_tryb = QHBoxLayout()
+        lbl_neapol = QLabel("Pizza Neapolitańska")
+        lbl_neapol.setFont(QFont(bakerie[0], 30))
+        lbl_neapol.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        pagelayout.addWidget(lbl_neapol, 0, 0, 1, 3, alignment=Qt.AlignCenter)
+
         lbl_tryb = QLabel("Tryb pieczenia")
         lbl_tryb.setFont(QFont(anta[0], 15))
         lbl_tryb.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        lbl_tryb.setScaledContents(
-            True)  # no nie dziala gowno, mialo robic ze font zmienia wielkosc przy rozciaganiu okna
-        lbl_tryb.setAlignment(QtCore.Qt.AlignLeft)
-        hbox_tryb.addWidget(lbl_tryb)
+        pagelayout.addWidget(lbl_tryb, 1, 0)
+
         self.toggle_button = QPushButton("Termoobieg", self)
         self.toggle_button.setCheckable(True)
+        self.toggle_button.setFont(QFont(anta[0], 9))
         self.toggle_button.clicked.connect(self.change_toggle)
-        hbox_tryb.addWidget(self.toggle_button)
-        pagelayout.addLayout(hbox_tryb)
+        pagelayout.addWidget(self.toggle_button, 1, 1, alignment=Qt.AlignCenter)
 
-        hbox_temp = QHBoxLayout()
-        lbl_temp = QLabel("Maksymalna temperatura")
-        lbl_temp.setFont(QFont(anta[0], 15))
-        lbl_temp.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        lbl_temp.setScaledContents(
-            True)  # no nie dziala gowno, mialo robic ze font zmienia wielkosc przy rozciaganiu okna
-        lbl_temp.setAlignment(QtCore.Qt.AlignLeft)
-        hbox_temp.addWidget(lbl_temp)
-
-        hbox_srednica = QHBoxLayout()
         lbl_srednica = QLabel("Średnica")
         lbl_srednica.setFont(QFont(anta[0], 15))
         lbl_srednica.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        lbl_srednica.setScaledContents(
-            True)  # no nie dziala gowno, mialo robic ze font zmienia wielkosc przy rozciaganiu okna
-        lbl_srednica.setAlignment(QtCore.Qt.AlignLeft)
-        hbox_srednica.addWidget(lbl_srednica)
+        pagelayout.addWidget(lbl_srednica, 2, 0)
 
         sld_srednica = QSlider(Qt.Orientation.Horizontal, self)
         sld_srednica.setRange(20, 40)
-        # nwm jak ustawic zeby to przesuwajace sie niebiezkie bylo innego koloru
         sld_srednica.setPageStep(1)
         sld_srednica.valueChanged.connect(self.updateLabel1)
-
-        sld_temp = QSlider(Qt.Orientation.Horizontal, self)
-        sld_temp.setRange(180, 280)
-        # nwm jak ustawic zeby to przesuwajace sie niebiezkie bylo innego koloru
-        sld_temp.setPageStep(5)
-        sld_temp.valueChanged.connect(self.updateLabel2)
+        pagelayout.addWidget(sld_srednica, 2, 1)
 
         self.lbl_slider_sr = QLabel('20', self)
         self.lbl_slider_sr.setAlignment(Qt.AlignmentFlag.AlignCenter |
                                         Qt.AlignmentFlag.AlignVCenter)
         self.lbl_slider_sr.setMinimumWidth(80)
         self.lbl_slider_sr.setFont(QFont(anta[0], 10))
+        pagelayout.addWidget(self.lbl_slider_sr, 2,2)
+
+        lbl_temp = QLabel("Maksymalna temperatura")
+        lbl_temp.setWordWrap(True)
+        lbl_temp.setFont(QFont(anta[0], 15))
+        lbl_temp.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        pagelayout.addWidget(lbl_temp, 3, 0)
+
+        sld_temp = QSlider(Qt.Orientation.Horizontal, self)
+        sld_temp.setRange(180, 280)
+        sld_temp.setPageStep(5)
+        sld_temp.valueChanged.connect(self.updateLabel2)
+        pagelayout.addWidget(sld_temp, 3, 1)
 
         self.lbl_slider_temp = QLabel('180', self)
         self.lbl_slider_temp.setAlignment(Qt.AlignmentFlag.AlignCenter |
                                           Qt.AlignmentFlag.AlignVCenter)
         self.lbl_slider_temp.setMinimumWidth(80)
         self.lbl_slider_temp.setFont(QFont(anta[0], 10))
-
-        hbox_srednica.addWidget(sld_srednica)
-        hbox_srednica.addSpacing(15)
-        hbox_srednica.addWidget(self.lbl_slider_sr)
-        pagelayout.addLayout(hbox_srednica)
-
-        hbox_temp.addWidget(sld_temp)
-        hbox_temp.addSpacing(15)
-        hbox_temp.addWidget(self.lbl_slider_temp)
-        pagelayout.addLayout(hbox_temp)
+        pagelayout.addWidget(self.lbl_slider_temp, 3, 2)
 
         btn = QPushButton("Wróć")
         btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         btn.setFont(QFont(bakerie[0], 20))
         btn.clicked.connect(self.close)
-        # btn.cliked.connect(self.go_back) #nwm czemu to nie dziala
-        pagelayout.addWidget(btn)
+        pagelayout.addWidget(btn, 4, 0, 1, 3, alignment=Qt.AlignCenter)
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
@@ -392,9 +378,9 @@ class MainNeapol(QMainWindow):
 
         sizegrip = QSizeGrip(central_widget)
         sizegrip.setStyleSheet("border: 1px solid black;")
-        pagelayout.addWidget(sizegrip, 0,
-                             QtCore.Qt.AlignBottom | QtCore.Qt.AlignRight)  # to right mowi w ktora str kursor jest skierowany chyba
+        pagelayout.addWidget(sizegrip, 5, 5, 1, 1, alignment=Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignRight)  # to right mowi w ktora str kursor jest skierowany chyba
         central_widget.setLayout(pagelayout)
+
 
     def updateLabel1(self, value):
         self.lbl_slider_sr.setText(str(value))
@@ -456,9 +442,11 @@ class MainRzym(QMainWindow):
         pagelayout.addWidget(sld_temp, 3, 1)
 
         self.lbl_slider_temp = QLabel('180', self)
+        self.lbl_slider_temp.setAlignment(Qt.AlignmentFlag.AlignCenter |
+                                          Qt.AlignmentFlag.AlignVCenter)
         self.lbl_slider_temp.setMinimumWidth(80)
         self.lbl_slider_temp.setFont(QFont(anta[0], 10))
-        pagelayout.addWidget(self.lbl_slider_temp, 3, 2, alignment=Qt.AlignmentFlag.AlignRight)
+        pagelayout.addWidget(self.lbl_slider_temp, 3, 2)
 
         btn = QPushButton("Wróć")
         btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -526,9 +514,11 @@ class MainAmeryka(QMainWindow):
         pagelayout.addWidget(sld_temp, 3, 1)
 
         self.lbl_slider_temp = QLabel('180', self)
+        self.lbl_slider_temp.setAlignment(Qt.AlignmentFlag.AlignCenter |
+                                        Qt.AlignmentFlag.AlignVCenter)
         self.lbl_slider_temp.setMinimumWidth(80)
         self.lbl_slider_temp.setFont(QFont(anta[0], 10))
-        pagelayout.addWidget(self.lbl_slider_temp, 3, 2, alignment=Qt.AlignmentFlag.AlignRight)
+        pagelayout.addWidget(self.lbl_slider_temp, 3, 2)
 
         btn = QPushButton("Wróć")
         btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
